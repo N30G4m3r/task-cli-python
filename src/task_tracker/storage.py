df5c6a1d2dd49448b1e0e188d9114d71786b451e
Leonardo Montilla <neogamer1211@gmail.com>
2026-03-16 16:45:51 -0400

    agregados mensajes de error pesonalisados a la hora de cargar los archivos json

--- src/task_tracker/storage.py
+++ src/task_tracker/storage.py
@@ -1,17 +1,28 @@
 import json
 import os
 
+# Códigos de color ANSI
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BOLD = "\033[1m"
+RESET = "\033[0m"
+
 # Persistencia (Lectura/Escritura JSON)
 def load_tasks(file_path="tasks.json"):
     if not os.path.exists(file_path):
         return []
-    with open(file_path, "r") as file:
-        return json.load(file)
+    try:
+        with open(file_path, "r") as file:
+            return json.load(file)
+    except Exception as e:
+        print(f"{RED}✗{RESET} {BOLD}Error loading tasks: {e}{RESET}")
+        exit(1)
 
 def save_tasks(tasks, file_path):
     try:
         with open(file_path, "w") as file:
             json.dump(tasks, file)
     except Exception as e:
-        print(f"Error saving tasks to {file_path}: {e}")
+        print(f"{RED}✗{RESET} {BOLD}Error saving tasks: {e}{RESET}")
         exit(1)
