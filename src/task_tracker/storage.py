aa6f662ee66bb415878624ffc7259d05abc7731c
Leonardo Montilla <neogamer1211@gmail.com>
2026-03-16 10:10:03 -0400

    creadas las funciones de almacenamiento de de las tareas

--- src/task_tracker/storage.py
+++ src/task_tracker/storage.py
@@ -1,1 +1,13 @@
+import json
+import os
+
 # Persistencia (Lectura/Escritura JSON)
+def load_tasks(file_path="tasks.json"):
+    if not os.path.exists(file_path):
+        return []
+    with open(file_path, "r") as file:
+        return json.load(file)
+
+def save_tasks(tasks, file_path):
+    with open(file_path, "w") as file:
+        json.dump(tasks, file)
