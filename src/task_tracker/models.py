abe1a64d51b6310ba058c5b7889bd8ac76091982
Leonardo Montilla <neogamer1211@gmail.com>
2026-03-16 17:04:28 -0400

    arreglo y comentarios de models

--- src/task_tracker/models.py
+++ src/task_tracker/models.py
@@ -2,12 +2,14 @@ import datetime
 
 # Definición de la estructura de la Tarea
 class Task:
+    # Constructor para inicializar una tarea con ID, descripción, estado y timestamps
     def __init__(self, id, description, status="todo"):
-        self.id = id
-        self.description = description
+        self.id = id # ID único de la tarea
+        self.description = description # Descripción de la tarea
         self.status = status # todo, in-progress, done
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = self.created_at
+        self.created_at = datetime.datetime.now().isoformat() # Timestamp de creación
+        self.updated_at = self.created_at # Timestamp de última actualización (inicialmente igual a created_at)
 
+    # Método para convertir la tarea a un diccionario (útil para JSON)
     def to_dict(self):
-        return self.__dict__
+        return self.__dict__ 
